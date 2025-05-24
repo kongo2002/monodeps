35863f487a06a1fecc660d5316f07e942968e3e7
kongo2002 <kongo2002@googlemail.com>
2025-05-24 12:42:55 +0200

    read input files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::collections::HashSet;
+
 use self::cli::Opts;
+
+use anyhow::Result;
 
 mod cli;
 mod config;
@@ -6,17 +10,32 @@ mod path;
 mod service;
 
 fn main() {
-    let opts = match Opts::parse() {
-        Ok(o) => o,
-        Err(err) => {
-            eprintln!("{}", err);
-            std::process::exit(1);
-        }
-    };
+    let opts = bail_out(Opts::parse());
+    let changed_files = bail_out(collect_changed_files());
 
     if let Ok(svs) = service::Service::discover(&opts.target.canonicalized) {
         for svc in svs {
             println!("{}", svc)
         }
     }
+}
+
+fn bail_out<T>(result: Result<T>) -> T {
+    match result {
+        Ok(inner) => inner,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn collect_changed_files() -> Result<HashSet<String>> {
+    let mut all = HashSet::new();
+
+    for line in std::io::stdin().lines() {
+        all.insert(line?);
+    }
+
+    Ok(all)
 }
